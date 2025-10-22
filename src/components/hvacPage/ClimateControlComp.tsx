5b6a14ed2eee956d756597af72bc4144e6a9bd91
Ibrahim Mohamed <Ibrahim_20210007@fci.helwan.edu.eg>
2025-10-22 06:57:30 +0300

    feat: finish the hvac page

---
+++ src/components/hvacPage/ClimateControlComp.tsx
@@ -0,0 +1,48 @@
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+export function ClimateControlComp({
+  partName,
+  imagesNumber,
+}: {
+  partName: string;
+  imagesNumber: number;
+}) {
+  const t = useTranslations(`hvacPage.${partName}`);
+
+  return (
+    <div className="flex flex-col gap-[1rem] flex-1">
+      <ul className="list-disc pl-6 text-white text-[1.125rem] font-normal leading-[1.8rem] ">
+        <li className="text-[1.5rem] font-[350]">{t(`header`)}</li>
+      </ul>
+      {(partName === "cracUnitsSection" ||
+        partName === "fanCollsUnitsSection") && (
+        <p className="text-white text-[1.25rem] font-[350] mb-[1.5rem]">
+          {t(`des`)}
+        </p>
+      )}
+      <div className="flex gap-[1rem] mt-auto">
+        {Array.from({ length: imagesNumber }, (_, i) => i + 1).map((num) => (
+          <div
+            key={num}
+            className="relative flex-1 rounded-[0.5rem] overflow-hidden max-w-[10rem]"
+          >
+            <Image
+              alt="image"
+              width={400}
+              height={400}
+              src={`/hvacPage/HeatingAndDHWAndClimateControlSection/${partName}/${num}.png`}
+              className="w-full"
+            />
+            {partName !== "cracUnitsSection" &&
+              partName !== "fanCollsUnitsSection" && (
+                <p className="absolute top-1/2 left-1/2 -translate-1/2 text-white text-[1.7rem] font-medium leading-[1.8rem] text-center">
+                  {t(`text${num}`)}
+                </p>
+              )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/hvacPage/HeatingAndDHWAndClimateControlSection.tsx
+++ src/components/hvacPage/HeatingAndDHWAndClimateControlSection.tsx
@@ -1,15 +1,47 @@
 import { useTranslations } from "next-intl";
 import { MostUsedHeader } from "./MostUsedHeader";
 import { HeatingAndDhwComp } from "./HeatingAndDhwComp";
+import { ClimateControlComp } from "./ClimateControlComp";
 
 export function HeatingAndDHWAndClimateControlSection() {
   const t = useTranslations("hvacPage.heatingAndDhwAndClimateControlSection");
+  const ClimateHeader = useTranslations("hvacPage.climateControlHeader");
   return (
     <div className="p-[var(--sectionPadding)]">
       <MostUsedHeader des={t("des")} header={t("header")} />
       <div className="flex  items-stretch gap-[9rem]">
         <HeatingAndDhwComp partName="heatingSection" />
         <HeatingAndDhwComp partName="dhwSection" />
+      </div>
+      <h1 className="text-[2rem] text-[#A8CF38] font-black my-[4rem]">
+        {ClimateHeader("header")}
+      </h1>
+      <div className="flex flex-col gap-[2rem]">
+        <div className="flex  items-stretch gap-[9rem]">
+          <ClimateControlComp partName="heatPumpSection" imagesNumber={3} />
+          <ClimateControlComp
+            partName="heatExchangerSection"
+            imagesNumber={3}
+          />
+        </div>
+        <div className="flex  items-stretch gap-[9rem]">
+          <ClimateControlComp partName="dehumidifierSection" imagesNumber={3} />
+          <ClimateControlComp
+            partName="airHandlingUnitsSection"
+            imagesNumber={3}
+          />
+        </div>
+        <div className="flex  items-stretch gap-[9rem]">
+          <ClimateControlComp partName="humidifierSection" imagesNumber={2} />
+          <ClimateControlComp partName="chillerSection" imagesNumber={2} />
+        </div>
+        <div className="flex  items-stretch gap-[9rem]">
+          <ClimateControlComp partName="cracUnitsSection" imagesNumber={1} />
+          <ClimateControlComp
+            partName="fanCollsUnitsSection"
+            imagesNumber={1}
+          />
+        </div>
       </div>
     </div>
   );
