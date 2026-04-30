c2b2f8c6a3066093d5665879302d39b895da9264
Ibrahim Mohamed <Ibrahim_20210007@fci.helwan.edu.eg>
2026-04-30 15:57:43 +0300

    h

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    trustHostHeader: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
